bec75df7e721a8b86cd3a7cebf75dae64298b493
salvadoreandradep <salvadorplay7089@gmail.com>
2022-09-07 21:19:02 -0600

    0.1

--- Form1.cs
+++ Form1.cs
@@ -8,26 +8,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
-using System.Data.SqlClient;
+
+
 
 
 namespace Login
 {
+
+
     public partial class Form1 : Form
     {
-
+        
         OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=[DataDirectory]\\login.accdb");
+        
         public Form1()
         {
             InitializeComponent();
+
         }
 
-      
+        
 
         private void Form1_Load(object sender, EventArgs e)
         {
             try
             {
+
+
 
                 conexion.Open();
                 MessageBox.Show("Conectada");
@@ -42,9 +49,10 @@ namespace Login
 
 
         }
-
+     
         private void button1_Click(object sender, EventArgs e)
         {
+            
             string consulta = "select password,usuario from login where password ='" + txtpass.Text + "' and usuario = '" + txtusuario.Text + "';";
             OleDbCommand comando = new OleDbCommand(consulta, conexion);
             OleDbDataReader leedb;
@@ -64,6 +72,11 @@ namespace Login
                 return;
             }
             conexion.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
         }
     }
   }
